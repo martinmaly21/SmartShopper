c71c4b4cf42827a75162317ee2df1c7341f5a6f8
Martin Maly <martinmaly66@hotmail.com>
2018-12-01 11:31:04 -0500

    Initial Commit

---
+++ SmartShopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartShopper
+//
+//  Created by Martin Maly on 2018-12-01.
+//  Copyright © 2018 Martin Maly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
