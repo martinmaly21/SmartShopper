d2dfe1ebfe824a9b1ae764d87e44912167d8f5ae
Martin Maly <martinmaly66@hotmail.com>
2018-12-01 18:34:14 -0500

    commit.

--- SmartShopper/ViewController.swift
+++ SmartShopper/ViewController.swift
@@ -20,20 +20,89 @@ class ViewController: UIViewController {
     @IBOutlet weak var shoppingCartButton: UIButton!
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var buyButton: UIButton!
+    @IBOutlet weak var thumbPicture: UIImageView!
+    var cardBehindHasBeenCreated = false
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpProductView()
         setUpButtons()
         setUpNavBar()
+        
     }
     
     @IBAction func clickedNo(_ sender: UIButton) {
-
+        
     }
     
     @IBAction func clickedYes(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        //user has touched card
+        
+    
+        if (cardBehindHasBeenCreated == false) {
+            
+            let newCard = UIView(frame: productView.frame)
+            newCard.backgroundColor = UIColor.red
+            //append new card to array
+            view.addSubview(newCard)
+            makeNextCardShow()
+            cardBehindHasBeenCreated = true
+        }
+        
+        
+        
+        
+        let card = sender.view!
+        //might need to change paramater below to 'view' from 'card'
+        let point = sender.translation(in: card)
+        let xFromCenter = card.center.x - view.center.x
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        
+        if xFromCenter > 0 {
+            thumbPicture.image = #imageLiteral(resourceName: "thumbUp.png")
+        } else {
+            thumbPicture.image = #imageLiteral(resourceName: "thumbDown.png")
+        }
+        
+        thumbPicture.alpha = abs((xFromCenter) / view.center.x)
+        
+        if sender.state == UIPanGestureRecognizer.State.ended {
+            if card.center.x < 75 {
+                //move off to left
+                UIView.animate(withDuration: 0.3) {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                }
+                cardBehindHasBeenCreated = false
+                return
+            }
+            else if card.center.x > (view.frame.width - 75) {
+                //move off to right
+                UIView.animate(withDuration: 0.3) {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                }
+                cardBehindHasBeenCreated = false
+                return
+            }
+            //why here?
+            UIView.animate(withDuration: 0.2) {
+                //animates same card back to middle if it is not changed,
+                card.center = self.view.center
+                self.thumbPicture.alpha = 0
+            }
+        }
+        
 
+        
+        
     }
     
     func setUpButtons() {
@@ -87,6 +156,16 @@ class ViewController: UIViewController {
         productView.bringSubviewToFront(productPrice)
         productView.bringSubviewToFront(productImage)
         productView.bringSubviewToFront(productRecomondation)
+        productView.bringSubviewToFront(thumbPicture)
     }
+    
+    func makeNextCardShow() {
+        //update cards images and shit
+        
+        
+    }
+    
+    //no categories but okay
+    // Also when user begins touch on view, then thats when the next card should appear.
 }
 
